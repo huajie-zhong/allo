d62ca89571155a9e1a7cda272579fc33747ac209
Huajie Zhong <hz.ansonzhong@gmail.com>
2024-11-19 00:40:46 -0500

    fix potential deadlock

--- tests/dataflow/test_1D_systolic.py
+++ tests/dataflow/test_1D_systolic.py
@@ -7,23 +7,23 @@ import allo.dataflow as df
 import allo.backend.hls as hls
 import numpy as np
 
-M, N, K = 16, 16, 16
+M, N, K = 2, 2, 2
 P0 = K + 2
 
 
 @df.region()
 def top():
-    fifo_A = df.array(df.pipe(dtype=float32, shape=(), depth=4), shape=(2, P0))
-    fifo_B = df.array(df.pipe(dtype=float32, shape=(), depth=4), shape=(2, P0))
+    fifo_A = df.array(df.pipe(dtype=float32, shape=(), depth=4), shape=(3, P0))
+    fifo_B = df.array(df.pipe(dtype=float32, shape=(), depth=4), shape=(3, P0))
 
-    @df.kernel(mapping=[2, P0])
+    @df.kernel(mapping=[3, P0])
     def gemm(A: float32[M, K], B: float32[K, N], C: float32[M, N]):
         i, j = df.get_pid()
-        with allo.meta_if(i == 0 and (j == 0 or j == P0 - 1)):
+        with allo.meta_if((i == 0 or i == 2) and (j == 0 or j == P0 - 1)):
             pass
         # input
         with allo.meta_elif(i == 0):
-            for _ in range(N):
+            for _ in range(M):
                 for k in range(K):
                     fifo_B[i + 1, j].put(B[k, j - 1])
         with allo.meta_elif(j == 0):
@@ -33,17 +33,22 @@ def top():
         # drain
         with allo.meta_elif(j == P0 - 1):
             for m in range(M):
-                for _ in range(K):
-                    a: float32 = fifo_A[i, j].get()
+                for k in range(K):
+                    fifo_A[i, j].get()
+        with allo.meta_elif(i == 2):
+            for _ in range(M):
+                for k in range(K):
+                    fifo_B[i, j].get()
         # compute
         with allo.meta_else():
             for m in range(M):
                 c: float32 = 0
-                for _ in range(K):
+                for k in range(K):
                     a: float32 = fifo_A[i, j].get()
                     b: float32 = fifo_B[i, j].get()
                     c += a * b
                     fifo_A[i, j + 1].put(a)
+                    fifo_B[i + 1, j].put(b)
                 C[m, j - 1] = c
 
 
@@ -51,7 +56,7 @@ def test_systolic():
     A = np.random.rand(M, K).astype(np.float32)
     B = np.random.rand(K, N).astype(np.float32)
     C = np.zeros((M, N), dtype=np.float32)
-    mod = df.build(top)
+    mod = df.build(top, target="vitis_hls", mode="hw", project="gemm.prj")
     if hls.is_available("vitis_hls"):
         mod(A, B, C)
         np.testing.assert_allclose(C, np.dot(A, B), atol=1e-5)
